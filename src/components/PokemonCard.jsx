2795e7bf7ac93b844b47bcb12de25ff222abd565
sem1n <songsm1129@gmail.com>
2025-10-16 13:19:57 +0900

    🌐한국어

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,18 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function PokemonCard({ pokemon }) {
-    return (
-        <Link
-            to={`/pokemon/${pokemon.id}`}
-            className="cursor-pointer bg-white shadow-md rounded-2xl p-4 flex flex-col items-center hover:scale-105 transition-transform"
-        >
-            <img 
-                src={`https://picsum.photos/192/192?random=${pokemon.id}`} 
-                alt={pokemon.name}
-                className="w-32 h-32 mb-2" 
-            />
-            <p className="text-lg font-bold">{pokemon.name}</p>
-            <p className="text-gray-500">#{pokemon.id}</p>
-        </Link>
-    )
+  return (
+    <Link
+      to={`/pokemon/${pokemon.id}`}
+      className="relative cursor-poiter bg-white shadow-md rounded-2x1 p-4 flex flex-col items-center hover:scale-105 transition-transform"
+    >
+      <div className="absoulute top-2 left-2 bg-gray-200 text-gray-700 text-xs font-semibold px-2 py-1 rounded-md">
+        {pokemon.id}
+      </div>
+
+      <img src={pokemon.image} alt={pokemon.name} className="w-32 h-32 mb-2" />
+      <p className="text-lg font-bold">{pokemon.name}</p>
+      {/* <p className="text-gray-500">#{pokemon.id}</p> */}
+    </Link>
+  );
 }
